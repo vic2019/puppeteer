f43ef3bfcb8fbff523a08ac4d492d8eee07576be
Victor <victor.fan.wang@gmail.com>
2020-03-14 00:00:27 -0700

    Edit index.js.

--- index.js
+++ index.js
@@ -1,43 +1,63 @@
 const puppeteer = require('puppeteer');
-
-(async () => {
-
-  let browser = await puppeteer.launch({
-      dumpio: true,
-      headless: true,
-      args: [
-          '--no-sandbox',
-          '--disable-setuid-sandbox',
-          '--disable-dev-shm-usage',
-      ]
-  });
-
-  console.log('browser starting');
-  const page = await browser.newPage();
-  console.log('browser started');
-  await page.goto('https://target.com/', {waitUntil:'domcontentloaded'});
-  console.log('page connected');
-  await page.waitFor(5000);
-  await page.screenshot({path: require('path').resolve(__dirname, 'example.png')});
-  console.log('page saved');
-  await browser.close();
-
-})();
-
 const http = require('http');
 const fs = require('fs');
-const PORT = process.env.PORT || 8080;
+const path = require('path');
+
+const PORT = process.env.PORT || 1337;
+let log = "Empty log";
 
 const server = http.createServer((req, res) => {
-  fs.readFile(__dirname + req.url, (err, data) => {
+  console.log(req.url);
+
+  if (req.url === '/log') {
+    res.writeHead(200, {'Content-Type': 'application/json'});
+    return res.end(JSON.stringify(log));
+  }
+
+  fs.readFile(path.resolve(__dirname, 'example.png'), (err, data) => {
     if (err) {
-      res.writeHead(404);
+      res.writeHead(404, {'Content-Type': 'application/json'});
       return res.end(JSON.stringify(err));
     }
 
     res.writeHead(200);
-    res.end(data);
+    return res.end(data);
   });
-}).listen(process.env.PORT || 8080);
+  
+});
 
-console.log("Server running at http://localhost:%d", PORT);
+
+(async () => {
+
+  try{
+    const browser = await puppeteer.launch({
+      dumpio: true,
+      headless: false,
+      args: [
+        '--no-sandbox',
+        '--disable-setuid-sandbox',
+        '--disable-dev-shm-usage',
+      ]
+    });
+
+    console.log('browser starting');
+    const page = await browser.newPage();
+    console.log('browser started');
+
+    await page.goto('https://target.com/', {waitUntil:'domcontentloaded'});
+    console.log('page connected');
+
+    await page.waitFor(6000);
+    await page.screenshot({path: path.resolve(__dirname, 'example.png')});
+    console.log('page saved');
+
+    await browser.close();
+
+  } catch(err) {
+    log = err;
+  }
+
+  server.listen(PORT);
+  console.log("Server listening on http://localhost:%d", PORT);
+
+})();
