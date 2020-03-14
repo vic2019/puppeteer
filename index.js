03f64e4a138bdc105169a974cf5d2f2b173dabbd
Victor <victor.fan.wang@gmail.com>
2020-03-13 17:21:21 -0700

    Initial commit.

---
+++ index.js
@@ -0,0 +1,25 @@
+const puppeteer = require('puppeteer');
+
+(async () => {
+
+  let browser = await puppeteer.launch({
+      dumpio: true,
+      headless: false,
+      args: [
+          '--no-sandbox',
+          '--disable-setuid-sandbox',
+          '--disable-dev-shm-usage',
+      ]
+  });
+
+  console.log('browser starting');
+  const page = await browser.newPage();
+  console.log('browser started');
+  await page.goto('https://github.com/', {waitUntil:'domcontentloaded'});
+  console.log('page connected');
+  await page.waitFor(5000);
+  await page.screenshot({path: require('path').resolve(__dirname, 'example.png')});
+  console.log('page saved');
+  await browser.close();
+
+})();
