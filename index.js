e76fdcbe4381d294784b650c5ffffb1710df2d50
Victor <victor.fan.wang@gmail.com>
2020-04-09 10:39:11 -0700

    Change default port to 8081.

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const http = require('http');
 const fs = require('fs');
 const path = require('path');
 
-const PORT = process.env.PORT || 1337;
+const PORT = process.env.PORT || 8081;
 const URL = process.env.URL || "https://www.wholefoodsmarket.com/";
 const log = fs.createWriteStream(__dirname + '/debug.log', { flags : 'w' });
 
